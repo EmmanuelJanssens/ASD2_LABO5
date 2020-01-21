c56c0a59f4a311c583bd9ca0db7e7dbe9ed81478
Unknown <ferraricorneloup.teo@gmail.com>
2020-01-21 18:21:06 +0100

    Trie

--- trie.h
+++ trie.h
@@ -2,28 +2,80 @@
 #define TRIE_H
 
 #include "interface.h"
-struct Node 
-{
-    struct Node* childs[26];
-    bool end;
+struct Node {
+	struct Node *left, *right, *middle;
+	char character;
+	bool end;
 };
 
 template<class keyType, class valueType>
-class Trie : data_interface<keyType,valueType>
-{
-        Node* _root;
+class Trie: data_interface<keyType, valueType> {
+	Node *_root;
 
-    public:
+	Node* get(Node *x, const valueType &word, int pos) {
+		if (x == nullptr) {
+			return nullptr;
+		}
+		char c = word[pos];
+		if (c < x->character) {
+			return get(x->left, word, pos);
+		} else if (c > x->character) {
+			return get(x->right, word, pos);
+		} else if (pos < word.length() - 1) {
+			return get(x->middle, word, pos + 1);
+		} else {
+			return x;
+		}
 
-        Trie(){}
-        ~Trie(){}
-        
+	}
 
-        void insert(const std::string& word) override{}
-        void remove(const std::string& word) override{}
+	Node* put(Node *x, const valueType &word, int pos) {
 
-        bool find(const std::string& word) override{return false;}
+		char c = word[pos];
+
+
+		if (x == nullptr) {
+			x = new Node();
+			x->character = c;
+			x->end = false;
+		}
+
+		if (c < x->character) {
+			x->left = put(x->left, word, pos);
+		} else if (c > x->character) {
+			std::cout << c <<x->character<< std::endl;
+			x->right = put(x->right, word, pos);
+		} else if (pos < word.length() - 1) {
+			x->middle = put(x->middle, word, pos + 1);
+		} else {
+			x->end = true;
+
+		}
+		return x;
+	}
+
+public:
+
+	Trie() {
+	}
+	~Trie() {
+	}
+
+	bool get(const valueType &word) override {
+		Node *x = get(_root, word, 0);
+		if (x == nullptr) {
+			return false;
+		}
+		if (x->end == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void put(const valueType &word) override {
+		_root = put(_root, word, 0);
+	}
 };
-
 
 #endif //TRIE_H
